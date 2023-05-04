76fe0108077c9e35dbaa84d734e80efe3cca18ea
鮫島泰史 jawssame7@gmail.com <jawssame7@gmail.com>
2023-05-04 14:34:41 +0900

    log add

--- resources/react/src/functions/Functions.ts
+++ resources/react/src/functions/Functions.ts
@@ -3,37 +3,34 @@
  * @param maxNum
  * @param perNumList
  */
-export const getNotHitList = (
-    maxNum: number,
-    ...perNumList: Array<number[]>
-) => {
-    // 集約関数
-    const lestArr = (...arr: Array<Array<number>>) => {
-        return arr;
-    };
+export const getNotHitList = (maxNum: number, ...perNumList: Array<number[]>) => {
+  // 集約関数
+  const lestArr = (...arr: Array<Array<number>>) => {
+    return arr;
+  };
 
-    let arr: Array<number> = [];
+  let arr: Array<number> = [];
 
-    lestArr(...perNumList).forEach((letArr) => {
-        arr = arr.concat(letArr);
-    });
+  lestArr(...perNumList).forEach((letArr) => {
+    arr = arr.concat(letArr);
+  });
 
-    // 重複を削除
-    let set: Set<number> = new Set(arr);
-    const uniqNumList = [...set];
+  // 重複を削除
+  let set: Set<number> = new Set(arr);
+  const uniqNumList = [...set];
 
-    let notHitList: Array<number> = [];
-
-    for (let i = 0; i < maxNum; i++) {
-        let num = i + 1;
-        if (uniqNumList.length > 0 && !uniqNumList.includes(num)) {
-            notHitList.push(num);
-        }
+  let notHitList: Array<number> = [];
+  console.log(uniqNumList);
+  for (let i = 0; i < maxNum; i++) {
+    let num = i + 1;
+    if (uniqNumList.length > 0 && !uniqNumList.includes(num)) {
+      notHitList.push(num);
     }
+  }
 
-    // 重複を削除
-    set = new Set(notHitList);
-    notHitList = [...set];
+  // 重複を削除
+  set = new Set(notHitList);
+  notHitList = [...set];
 
-    return notHitList;
+  return notHitList;
 };
