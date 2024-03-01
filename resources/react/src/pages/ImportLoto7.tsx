dc8b8b08b50be17af838c78efb2ffc224950df54
鮫島泰史 <h.sameshima@wiz-net.jp>
2024-03-01 14:07:11 +0900

    csvインポート ファイル名を表示させるように修正

--- resources/react/src/pages/ImportLoto7.tsx
+++ resources/react/src/pages/ImportLoto7.tsx
@@ -29,15 +29,19 @@ const ImportLot7 = () => {
       <div className={'flex flex-col flex-1 px-3'}>
         <h1 className={'text-2xl font-bold'}>Loto7</h1>
         <div>
-          <div className="py-6 mx-auto max-w-screen-xl">
-            <div className="max-w-7xl mx-auto">
-              <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div className="p-6 text-gray-900">
-                  <div {...getRootProps({ className: 'dropzone' })}>
-                    <input {...getInputProps()} />
-                    <p>ドラッグアンドドロップするかクリックしてファイルを選択してください</p>
-                  </div>
-                </div>
+          <div className={'py-6 mx-auto max-w-screen-xl'}>
+            <div className={'max-w-7xl mx-auto'}>
+              <div
+                {...getRootProps({
+                  className:
+                    'dropzone p-6 bg-white overflow-hidden shadow-sm sm:rounded-lg outline-dashed outline-blue-500/50 text-gray-900',
+                })}
+              >
+                <input {...getInputProps()} />
+                <p>ドラッグアンドドロップするかクリックしてファイルを選択してください</p>
+              </div>
+              <div className={'p-6'}>
+                <div>{data.csvFile?.path}</div>
               </div>
             </div>
             <label className={'input-group'}>
